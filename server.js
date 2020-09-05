c1215a4eaf8d1139fd9cc5b1d3f5733b02cf79f0
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-09-05 15:06:52 -0400

    DEAD_SIMPLE_ROUTING

--- server.js
+++ server.js
@@ -3,9 +3,23 @@
 
     var server=http.createServer( 
     (server_req,server_res)=>{
-    
-        server_res.write("[hello_world]");
-        server_res.end();
+
+        //:DEAD_SIMPLE_ROUTING:------------------------------://
+
+        const url_cap=( server_req.url.toUpperCase() );
+        if( url_cap == "/" ){
+            server_res.write("[site_root]");
+            server_res.end();
+        }else
+        if( url_cap == "/STATIC/HELLO.TXT" ){
+            server_res.write("[TODO:hello.txt]");
+            server_res.end();
+        }else{
+            server_res.write("[BAD_ROUTE]");
+            server_res.end();
+        };;
+
+        //:------------------------------:DEAD_SIMPLE_ROUTING://
     
     });;
     server.listen( process.env.PORT );
